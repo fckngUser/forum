cf2a3b0c8b7d0542254632cfe7e802b6a637bdb1
hako <ruslancheg5@gmail.com>
2025-05-22 20:50:31 +0300

    article creation

--- forumApp/views.py
+++ forumApp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from forumApp.models import Articles
 
-# Create your views here.
+def main_page(request):
+    articles = Articles.objects.all().order_by('-id')  
+    return render(request, 'main_page.html', {'articles': articles})
+
+def create_article_page(request):
+    if request.method == "POST":
+        name = request.POST.get("name")  
+        content = request.POST.get("content")  
+        
+        if name and content:  
+            Articles.objects.create(name=name, content=content)
+            return redirect("/")
+    
+    return render(request, 'create_article.html')
