2dd3147cdb9aed0e0855c03009077a19148edbf8
hako <ruslancheg5@gmail.com>
2025-05-23 18:40:09 +0300

    create authenticate

--- forum/urls.py
+++ forum/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main_page, name='main_page'),
     path('create/', views.create_article_page, name='create_article'),
-    path('login/', views.login_page, name='login')
+    path('login/', views.login_page, name='login'),
+    path('register/', views.register_page, name='register'),
+    path('logout/', views.logout_page, name='logout'),
+    path('profile/', views.profile_page, name='profile'),
 ]
--- forumApp/views.py
+++ forumApp/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, redirect
 from forumApp.models import Articles
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+
+
+
 
 def main_page(request):
     articles = Articles.objects.all().order_by('-id')  
     return render(request, 'main_page.html', {'articles': articles})
+
+
 
 def create_article_page(request):
     if request.method == "POST":
@@ -16,6 +24,50 @@ def create_article_page(request):
     
     return render(request, 'create_article.html')
 
+
+
 def login_page(request):
-    context = {}
-    return render(request, "registration/login.html", context)
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, 'invalid username or password')
+        
+    return render(request, "registration/login.html")
+
+
+
+def logout_page(request):
+    logout(request)
+    return redirect('login')
+
+
+
+def register_page(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        
+        if password == confirm_password:
+            try:
+                user = User.objects.create_user(username=username, password=password)
+                user.save()
+                login(request, user)
+                return redirect('home')
+            except:
+                messages.error(request, 'Username already exists')
+                
+        else:
+            messages.error(request, 'Passwords do not match')
+    return render(request, 'registration/register.html')
+
+
+
+def profile_page(request):
+    return render(request, 'profile.html')
