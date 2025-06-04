8ffa6e24c38ad8a5a6903e648639f2901d1b9944
hako <ruslancheg5@gmail.com>
2025-06-04 23:09:08 +0300

    deployment preparation and ya ebal

--- forum/settings.py
+++ forum/settings.py
@@ -82,4 +82,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
